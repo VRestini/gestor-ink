14a528ba8e348aa8fc24f485b6081b72c5c6c60c
Vitor <vitorsouzalibrelonrestini@gmail.com>
2024-08-29 15:08:33 -0300

    a

--- AppGestorInk/AppGestorInk/MVVM/Popups/AddItemPop.xaml.cs
+++ AppGestorInk/AppGestorInk/MVVM/Popups/AddItemPop.xaml.cs
@@ -11,4 +11,29 @@ public partial class AddItemPop : ContentPage
         BindingContext = addItemViewModel;
         datePicker.Date = DateTime.Now;
     }
+    private async void EditorWithoutBorder_Unfocused(object sender, FocusEventArgs e)
+    {
+
+        BorderPrice.Stroke = Colors.LightGray;
+        await BorderPrice.ScaleTo(1, 100, Easing.CubicOut);
+    }
+
+    private async void EditorWithoutBorder_Focused(object sender, FocusEventArgs e)
+    {
+        BorderPrice.Stroke = Color.FromArgb("#4C007D");
+        await BorderPrice.ScaleTo(1.05, 100, Easing.CubicIn);
+    }
+
+    private async void EntryWithoutBorder_Unfocused(object sender, FocusEventArgs e)
+    {
+        BorderQtd.Stroke = Colors.LightGray;
+        await BorderQtd.ScaleTo(1, 100, Easing.CubicOut);
+    }
+
+    private async void EntryWithoutBorder_Focused(object sender, FocusEventArgs e)
+    {
+
+        BorderQtd.Stroke = Color.FromArgb("#4C007D");
+        await BorderQtd.ScaleTo(1.05, 100, Easing.CubicIn);
+    }
 }
