a35aa590573f6c28af11ca722be848c1ac51ab65
VitorRestini <vitorsouzalibrelonrestini@gmail.com>
2024-10-01 22:20:40 -0300

    a

--- AppGestorInk/AppGestorInk/MVVM/Views/Agenda.xaml.cs
+++ AppGestorInk/AppGestorInk/MVVM/Views/Agenda.xaml.cs
@@ -2,16 +2,25 @@
 using AppGestorInk.MVVM.ViewModels;
 using Syncfusion.Maui.Calendar;
 using Syncfusion.Maui.Scheduler;
+using System.Runtime.CompilerServices;
 
 namespace AppGestorInk.MVVM.Views;
 
 public partial class Agenda : ContentPage
 {
-   
-    public Agenda(AgendaViewModel agendaViewModel)
+  
+    public CalendarTextStyle TextStyle { get; set; }
+    public Agenda()
     {
         InitializeComponent();
-        BindingContext = agendaViewModel;
+        CalendarTextStyle textStyle = new CalendarTextStyle()
+        {
+            TextColor = Colors.White,
+            
+        };
+        
+        this.MyCalendar.MonthView.TextStyle = textStyle;
+        
     }
     
 
